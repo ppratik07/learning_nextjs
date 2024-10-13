746d047f92289f8d454c7587336d57e4cc5b01b5
pratikprajapati0708 <prajapatipratik292@gmail.com>
2024-10-13 21:57:03 +0530

    Added in page.tsx

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,3 +1,10 @@
+import { NavbarDemo } from "@/components/Navbar";
+
 export default function Home() {
-  return <h1 className="text-3xl font-bold underline text-red-500">Hello world!</h1>;
+  return(
+      <div>
+        <NavbarDemo/>
+      </div>
+  );
 }
+
