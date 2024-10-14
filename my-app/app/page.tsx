8a2b5d741a65fc2f262b6383624d33c15c07c94e
pratikprajapati0708 <prajapatipratik292@gmail.com>
2024-10-14 12:04:20 +0530

    Added 3d Pin

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,9 +1,11 @@
+import { AnimatedPinDemo } from "@/components/AnimatedPin";
 import { NavbarDemo } from "@/components/Navbar";
 
 export default function Home() {
   return(
       <div>
         <NavbarDemo/>
+        <AnimatedPinDemo/>
       </div>
   );
 }
