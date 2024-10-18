83d10726f240299ac021becb33c47df684400134
pratikprajapati0708 <prajapatipratik292@gmail.com>
2024-10-18 22:00:11 +0530

    updated main page

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,5 +1,6 @@
 import { AnimatedPinDemo } from "@/components/AnimatedPin";
 import { FloatingDockDemo } from "@/components/FloatingDock";
+import { GoogleGeminiEffectDemo } from "@/components/Gemini";
 import { NavbarDemo } from "@/components/Navbar";
 
 export default function Home() {
@@ -8,6 +9,7 @@ export default function Home() {
         <NavbarDemo/>
         <AnimatedPinDemo/>
         <FloatingDockDemo/>
+        <GoogleGeminiEffectDemo/>
       </div>
   );
 }
